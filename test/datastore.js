91f293b13bd8723395933b0341a82a92ebf64852
Daniel Lewis <github@mrdaniellewis.co.uk>
2016-03-07 20:07:25 +0000

    Add error handling tests

--- test/datastore.js
+++ test/datastore.js
@@ -67,6 +67,34 @@ describe( 'datastore', function() {
 
         } );
 
+        it( 'errors if elastic search cannot be found', function() {
+
+            const errorConfig = {
+                index,
+                config: {
+                    host: 'invalid:9200',
+                },
+            };
+
+            const datastore = new Datastore( errorConfig );
+            return datastore.addReference( {
+                url: 'http://www.bbc.co.uk/test',
+                source: {
+                    nodeName: 'a',
+                    attributes: { href: '/test' },
+                    base: undefined,
+                    url: 'http://www.bbc.co.uk/',
+                    type: 'html',
+                },
+            } )
+                .then( () => {
+                    throw new Error( 'Should not have been called' );
+                } )
+                .catch( e => {
+                    expect( e.message ).toEqual( 'No Living connections' );
+                } );
+
+        } );
 
     } );
 
@@ -86,9 +114,36 @@ describe( 'datastore', function() {
 
         } );
 
+        it( 'errors if elastic search cannot be found', function() {
+
+            const errorConfig = {
+                index,
+                config: {
+                    host: 'invalid:9200',
+                },
+            };
+
+            const datastore = new Datastore( errorConfig );
+            return datastore.addReference( {
+                url: 'http://www.bbc.co.uk/test',
+                source: {
+                    nodeName: 'a',
+                    attributes: { href: '/test' },
+                    base: undefined,
+                    url: 'http://www.bbc.co.uk/',
+                    type: 'html',
+                },
+            } )
+                .then( () => {
+                    throw new Error( 'Should not have been called' );
+                } )
+                .catch( e => {
+                    expect( e.message ).toEqual( 'No Living connections' );
+                } );
+
+        } );
+
 
     } );
-
-    
 
 } );
